7515213319ece3d55c5c46742644ac97fdbcf575
Honassis Lopes <honassislopes@gmail.com>
2020-03-27 03:17:16 -0300

    Updates

---
+++ precache-manifest.825d7da54d33c9f6ff0a9d31543c99be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87d52a88b644e29f671481735347fc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12be00f0fb5d3e7ac4b3",
+    "url": "/static/css/main.6ec5e36a.chunk.css"
+  },
+  {
+    "revision": "e0c698fde612bdd95836",
+    "url": "/static/js/2.9ede9a78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9ede9a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12be00f0fb5d3e7ac4b3",
+    "url": "/static/js/main.0863ba62.chunk.js"
+  },
+  {
+    "revision": "a9826cacf95ce8ad1ce1",
+    "url": "/static/js/runtime-main.6e4d6195.js"
+  },
+  {
+    "revision": "68aea7c46d89cc9dec8fce0fc483443f",
+    "url": "/static/media/heroes.68aea7c4.png"
+  },
+  {
+    "revision": "d34a752f54254ad9347e009a072053da",
+    "url": "/static/media/logo.d34a752f.svg"
+  }
+]);
